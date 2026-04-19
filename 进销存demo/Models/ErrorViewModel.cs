eb9b1238bb8f48a5449592bb3318c8cbe0d2ed92
Sha7dow <2803861918@qq.com>
2026-04-19 19:35:34 +0800

    init Project

---
+++ 进销存demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace 进销存demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
